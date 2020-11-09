5775e2b12e710e9ebb1739fdf240966b73a848ef
Mavis Francia <mfrancia@bluebeam.com>
2020-11-09 17:05:10 -0600

    Prettier formatting

--- create_and_validate_cog.js
+++ create_and_validate_cog.js
@@ -1,7 +1,7 @@
-const calibrationConfigs = require('./calibrationConfigs.json');
+const calibrationConfigs = require("./calibrationConfigs.json");
 
-const input = 'site_plan_lg.png';
-const output = 'site_plan.tif';
+const input = "site_plan_lg.png";
+const output = "site_plan.tif";
 const width = 16800;
 const height = 12000;
 
@@ -13,25 +13,38 @@ gdal_translate -of GTiff -a_srs EPSG:4326 \
   -gcp ${width} 0 ${b[1].lng} ${b[1].lat} \
   -gcp ${width} ${height} ${b[2].lng} ${b[2].lat} \
   -gcp 0 ${height} ${b[3].lng} ${b[3].lat} \
-  /app/${input} /app/${output.replace('.tif', '_intermediate.tif')}
+  /app/${input} /app/${output.replace(".tif", "_intermediate.tif")}
 `.trim();
 
-console.log('=== Step 1: Create Geotiff ===');
-console.log(`docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${cmd}"`);
-console.log('');
+console.log("=== Step 1: Create Geotiff ===");
+console.log(
+  `docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${cmd}"`
+);
+console.log("");
 
-console.log('=== Step 2: Flatten Geotiff ===');
-const flattenCmd = `gdalwarp -t_srs EPSG:4326 /app/${output.replace('.tif', '_intermediate.tif')} /app/${output}`;
-console.log(`docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${flattenCmd}"`);
-console.log('');
+console.log("=== Step 2: Flatten Geotiff ===");
+const flattenCmd = `gdalwarp -t_srs EPSG:4326 /app/${output.replace(
+  ".tif",
+  "_intermediate.tif"
+)} /app/${output}`;
+console.log(
+  `docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${flattenCmd}"`
+);
+console.log("");
 
-console.log('=== Step 3: Convert to COG ===');
+console.log("=== Step 3: Convert to COG ===");
 console.log(`
-docker run -v $(pwd):/app rio-cogeo:latest bash -c "rio cogeo create --web-optimized --add-mask --allow-intermediate-compression --overview-resampling average --resampling average ${output} ${output.replace('.tif', '_cog.tif')}"
+docker run -v $(pwd):/app rio-cogeo:latest bash -c "rio cogeo create --web-optimized --add-mask --allow-intermediate-compression --overview-resampling average --resampling average ${output} ${output.replace(
+  ".tif",
+  "_cog.tif"
+)}"
 `);
-console.log('');
+console.log("");
 
-console.log('=== Step 2: Validate COG ===');
+console.log("=== Step 2: Validate COG ===");
 console.log(`
-docker run -v $(pwd):/app rio-cogeo:latest bash -c "rio cogeo validate ${output.replace('.tif', '_cog.tif')}"
+docker run -v $(pwd):/app rio-cogeo:latest bash -c "rio cogeo validate ${output.replace(
+  ".tif",
+  "_cog.tif"
+)}"
 `);
--- create_vrt_then_tile.js
+++ create_vrt_then_tile.js
@@ -1,6 +1,6 @@
-const input = '/app/site_plan_lg.png';
-const vrtPath = input.replace('.png', '.vrt');
-const outputPath = '/app/tiles';
+const input = "/app/site_plan_lg.png";
+const vrtPath = input.replace(".png", ".vrt");
+const outputPath = "/app/tiles";
 const width = 16800;
 const height = 12000;
 
@@ -15,11 +15,15 @@ gdal_translate -of VRT -a_srs EPSG:4326 \
   ${input} ${vrtPath}
 `.trim();
 
-console.log('=== Step 1: Create VRT ===');
-console.log(`docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${cmd}"`);
-console.log('');
+console.log("=== Step 1: Create VRT ===");
+console.log(
+  `docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${cmd}"`
+);
+console.log("");
 
-console.log('=== Step 2: Build Tiles ===');
-const tileCmd = `gdal2tiles.py --profile=mercator --processes=4 --zoom=10-22 --exclude --tilesize=512 --xyz --webviewer=none ${vrtPath} ${outputPath}`
-console.log(`docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${tileCmd}"`);
-console.log('');
+console.log("=== Step 2: Build Tiles ===");
+const tileCmd = `gdal2tiles.py --profile=mercator --processes=4 --zoom=10-22 --exclude --tilesize=512 --xyz --webviewer=none ${vrtPath} ${outputPath}`;
+console.log(
+  `docker run -v $(pwd):/app osgeo/gdal:ubuntu-full-3.1.2 bash -c "${tileCmd}"`
+);
+console.log("");
